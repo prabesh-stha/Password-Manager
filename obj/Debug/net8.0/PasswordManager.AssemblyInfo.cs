4d493cf0b0dcca424e0609c5d0e49b8bfde8d362
Prabesh Shrestha <shresthaprabesh155@gmail.com>
2024-07-22 21:14:23 +0545

    First Commit

--- obj/Debug/net8.0/PasswordManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/PasswordManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89d201826a1299672f104051804601066385449d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
